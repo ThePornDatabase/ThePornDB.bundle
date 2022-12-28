3bc9ef7614fb529b690aa2ea8b8cb888c1edf8d8
Darklyter <briadin@yahoo.com>
2022-12-27 22:01:09 -0500

    Redoing the regex to work with existing options, and fixing those options
    Figured out how to fix the already existing "Use filepath" option, so I just worked the expanded regex filter into that

--- ThePornDBScenes.bundle/Contents/Code/__init__.py
+++ ThePornDBScenes.bundle/Contents/Code/__init__.py
@@ -3,7 +3,7 @@ import urllib
 from dateutil.parser import parse
 
 API_BASE_URL = 'https://api.metadataapi.net'
-API_SEARCH_URL = API_BASE_URL + '/scenes?parse=%s&hash=%s'
+API_SEARCH_URL = API_BASE_URL + '/scenes?q=%s&hash=%s'
 API_SCENE_URL = API_BASE_URL + '/scenes/%s'
 API_SITE_URL = API_BASE_URL + '/sites/%s'
 
@@ -40,13 +40,31 @@ class ThePornDBScenesAgent(Agent.Movies):
     def search(self, results, media, lang):
         open_hash = ''
         if media.openSubtitlesHash and Prefs['oshash_matching_enable']:
-            open_hash = media.openSubtitlesHash
-
+            open_hash = media.items[0].parts[0].openSubtitleHash
+        
         title = media.name
-        if media.filename and Prefs['match_by_filepath_enable']:
-            title = urllib.unquote(media.filename)
+        filename = re.search('.*[\\\/](.*)', media.items[0].parts[0].file).group(1)
+        filename = re.search('(.*)\.\w{3,4}', filename).group(1)
+        if filename and Prefs['match_by_filepath_enable']:
+            title = urllib.unquote(filename)
             if Prefs['filepath_cleanup_enable'] and Prefs['filepath_cleanup']:
-                title = re.sub(Prefs['filepath_cleanup'], '', title)
+                regexes = Prefs['filepath_cleanup'].split(",")
+                Log('[TPDB Agent] Regexes Passed: `%s`' % Prefs['filepath_cleanup'])
+                Log('[TPDB Agent] Title before Regex: `%s`' % title)
+                for regex in regexes:
+                    try:
+                        regexcompile = re.compile(regex, re.IGNORECASE)
+                        if re.search(regexcompile, title):
+                            Log('[TPDB Agent] Stripping Regex from Title: `%s`' % regex)
+                            title = regexcompile.sub("", title).strip()
+                    except re.error:
+                        Log('[TPDB Agent] Invalid Regex supplied: `%s`' % regex)
+                Log('[TPDB Agent] Title after Regex: `%s`' % title)                
+                
+        if Prefs['rewrite_season_episode']:
+            if re.search(' ([sS]\d+[eE]\d+) ', title) or re.search(' ([sS]\d+[eE]\d+) ?', title):
+                titlesearch = re.search('(.* [sS]\d+)([eE]\d+ ?.*)', title)
+                title = titlesearch.group(1) + ":" + titlesearch.group(2)
 
         search_results = []
         if title:
